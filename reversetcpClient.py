5b13be78dd3ed6dc9f76dc933c0c68eec6dc0320
AstrologerLuo <astrologerluo@gmail.com>
2025-06-04 09:26:43 +0800

    send all codes

---
+++ reversetcpClient.py
@@ -0,0 +1,49 @@
+import socket
+import random
+import struct
+server_ip = input("Server ip: ")
+server_port = int(input("Server port: "))
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((server_ip, server_port))
+f = open('send.txt', 'rb')
+new_f=open("new.txt","w",encoding="utf-8")
+chunk_num=0   #块的数目
+chunks=[]       #块的内容形成的列表
+chunks_len=[]   #每一块的大小
+lmin=int(input("请输入一块的最小值（单位B）："))
+lmax=int(input("请输入一块的最大值（单位B）："))
+while True:
+    chunk_size=random.randint(lmin,lmax)    #将文件随机划分成10B到20B
+    chunks_len.append(chunk_size)
+    chunk=f.read(chunk_size)
+    if not chunk:
+        break
+    chunks.append(chunk)
+    chunk_num+=1
+initialization=struct.pack('>HI',1,chunk_num)   #大端序也就是网络序
+client.sendall(initialization)
+agree_data = client.recv(2)
+agree = struct.unpack('>H', agree_data)[0] #返回的是元组,[0]提取其中第一个值
+if agree != 2:
+    raise ValueError("没有收到agree报文")#主动报错并终止程序
+ans_total=''
+for i in range(0,chunk_num):
+    header=struct.pack('>HI',3,chunks_len[i])
+    send_data=header+chunks[i]
+    client.sendall(send_data)
+    reverse=client.recv(6+chunks_len[i])
+    reverse_header=struct.unpack('>HI', reverse[:6])
+    if reverse_header[0] != 4:
+        raise ValueError("接收到reverseAnswer报文错误") #主动报错并终止程序
+    reverse_data=reverse[6:6+chunks_len[i]]
+    ans_str=reverse_data.decode('utf-8')
+    ans_total = ans_str + ans_total
+    print(f'第{i+1}块：{ans_str}')
+ans_total = ''.join(line  # 保留的每一行内容
+                    for line in ans_total.splitlines(keepends=True)  # 按行分割（保留换行符）
+                    if line.strip()) # 只保留非空行（去除纯空白字符的行） line.strip() 返回空字符串 ""，则 if 条件为 False，该行被过滤。
+new_f.write(ans_total)
+input("点击回车结束结束")
+f.close()
+new_f.close()
+client.close()
---
+++ reversetcpServer.py
@@ -0,0 +1,37 @@
+import socket
+import threading
+import struct
+
+def handle_client(conn, addr):
+    init = conn.recv(6)
+    init_data, n = struct.unpack('>HI', init)
+    if init_data != 1:
+        print(f"{addr}的initialization报文错误")
+        conn.close()
+        return
+    agree=struct.pack('>H',2)
+    conn.sendall(agree)
+    for i in range(n):
+        req=conn.recv(6)
+        req_type,req_len=struct.unpack('>HI',req)
+        if req_type!=3:
+            print(f"{addr}的reserveRequest报文错误")
+            conn.close()
+            return
+        req_data=conn.recv(req_len)
+        req_str=req_data.decode('UTF-8')#变为UTF-8编码的字符串
+        ans_str=''.join(reversed(req_str)) #翻转
+        ans_data=ans_str.encode('UTF-8')#翻转之后再变为bytes
+        header=struct.pack('>HI',4,req_len)
+        ans=header+ans_data
+        conn.sendall(ans)
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(('127.0.0.1', 1616))
+server.listen(5)
+print("服务器启动，等待连接...")
+while True:
+    conn, addr = server.accept()
+    thread = threading.Thread(target=handle_client, args=(conn, addr))#线程要执行的函数  传给函数的参数
+    thread.start()
+    print(f"活跃连接数: {threading.active_count() - 1}")
